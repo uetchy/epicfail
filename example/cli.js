e24ca90868bdcb63b79a2100c31c80572d5d7fd6
Yasuaki Uechi <y@uechi.io>
2020-09-01 17:36:52 +0900

    chore: update example

--- example/cli.js
+++ example/cli.js
@@ -1,17 +1,8 @@
 #!/usr/bin/env node
 
 const handleErrors = require('epicfail').default;
-const Sentry = require('@sentry/node');
 const fs = require('fs');
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  defaultIntegrations: false,
-});
-
-handleErrors({
-  showStackTrace: false,
-  onError: Sentry.captureException, // will returns event_id issued at Sentry
-});
+handleErrors();
 
 fs.readFileSync('foo');
